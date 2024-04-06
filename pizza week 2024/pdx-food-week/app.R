1c192f54c35063b888fabf830e179ae2f8dfc180
jnjahncke <jennifer.jahncke@gmail.com>
2024-04-06 15:48:26 -0700

    pizza week 2024

---
+++ pizza week 2024/pdx-food-week/app.R
@@ -0,0 +1,215 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(sf)
+library(plotly)
+library(DT)
+
+load("for_shiny.RData")
+pizza_week <- pizza_week %>% mutate(interest_level = 5)
+
+# combo meat_veg with veggie_sub and vegan_sub
+
+# combo gf with gf_sub
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Pizza Week 2024"),
+    
+    # Sidebar
+    sidebarLayout(
+        sidebarPanel(
+            actionButton("go", label = "Update Map"),
+            sliderInput(inputId = "days", label = "How many days long is your pizza crawl?", min = 1, max = 7, value = 7),
+            sliderInput(inputId = "vote",label = "Only show pizzas I am interested in. Interest threshold:", min = 1, max = 5, value = 3),
+            selectInput(inputId = "meat_veg", label = "Meat or Vegetarian?", choices = c("Meat", "Vegetarian", "Vegan", "All"), selected = "All", multiple = FALSE),
+            selectInput(inputId = "gluten", label = "Gluten free?", choices = c("Yes", "No", "All"), selected = "All", multiple = FALSE),
+            selectInput(inputId = "slice_pie", label = "Slice or Whole Pie?", choices = c("Slice", "Whole Pie", "Either"), selected = "Either", multiple = FALSE),
+            selectInput(inputId = "takeout", label = "Dine In or Takeout?", choices = c("Takeout", "Either"), selected = "Either", multiple = FALSE),
+            selectInput(inputId = "delivery", label = "Offers Delivery?", choices = c("Delivery", "Don't Care"), selected = "Don't Care", multiple = FALSE),
+            selectInput(inputId = "minors", label = "Allows Minors?", choices = c("Yes", "No", "Either"), selected = "Either", multiple = FALSE),
+            checkboxGroupInput(inputId = "disp_cols", label = "Choose which columns to display:", choices = c("restaurant","pizza","toppings","address","interest_level","cluster","hours","meat_veggie","veggie_sub","vegan_sub","gf","gf_sub","whole_slice","minors","takeout","delivery","purchase_limit","availability_limit"),
+                selected = c("restaurant","pizza","toppings","address","interest_level","cluster")),
+            width = 2), # designate sidebar width
+        
+        
+        # Main Panel
+        mainPanel(
+            
+            plotlyOutput("map"),
+            
+            tabsetPanel(
+                tabPanel("Pizzas",
+                         downloadButton('download1', "Download Table"),
+                         DTOutput("pizza_db")),
+                
+                tabPanel("Cast Your Votes",
+                         downloadButton('download2', "Download Votes"),
+                         DTOutput("vote_db")),
+                
+                tabPanel("View Hours",
+                         downloadButton('download3', "Download Hours"),
+                         dataTableOutput("schedule")),
+                
+                tabPanel("Instructions",
+                         includeHTML("instructions.html")))
+        
+                
+                )))
+
+
+
+
+server <- function(input, output) {
+    
+    
+    ########################### VOTES ########################### 
+    
+    # download button for votes
+    output$download2 <- downloadHandler(filename = function() {"votes.csv"},
+                                        content = function(fname) {write.csv(v$data %>% select(restaurant, pizza, toppings, address, interest_level), fname)})
+    
+    # for casting votes
+    v <- reactiveValues(data = pizza_week %>% select(restaurant, pizza, toppings, address, interest_level))
+    
+    
+    # display vote datatable
+    output$vote_db <- renderDT({
+        
+        v$data %>%
+            datatable(editable = TRUE)
+    })
+    
+    # track changes
+    observeEvent(input$vote_db_cell_edit, {
+        #get values
+        info = input$vote_db_cell_edit
+        i = as.numeric(info$row)
+        j = as.numeric(info$col)
+        k = as.numeric(info$value)
+        
+        #write values to reactive
+        v$data[i,j] <- k
+    })
+    
+    
+    
+    ########################### FILTERED TABLE ########################### 
+    slice_pie <- reactive({
+        if (input$slice_pie == "Slice") {
+            return("Slice|Both")
+        } else if (input$slice_pie == "Whole Pie") {
+            return("Whole|Both")
+        } else {
+            return(".")
+        }
+    })
+    
+    # filter, cluster
+    filter_db <- reactive({
+        votes <- pizza_week %>% select(-interest_level) %>% 
+            filter(
+                grepl(pattern = ifelse(input$meat_veg == "All", ".", input$meat_veg), x = meat_veggie),
+                grepl(pattern = ifelse(input$gluten == "All", ".", input$gluten), x = gf_sub),
+                grepl(pattern = slice_pie(), x = whole_slice),
+                grepl(pattern = ifelse(input$takeout == "Takeout", "Yes", "."), x = takeout),
+                grepl(pattern = ifelse(input$delivery == "Delivery", "Yes", "."), x = delivery),
+                grepl(pattern = ifelse(input$minors == "Either", ".", input$minors), x = minors)) %>% 
+            inner_join(v$data) %>% 
+            filter(interest_level >= input$vote)
+            
+        
+        # clusters
+        coords_scale <- votes %>% select(latitude, longitude) %>% scale()
+        km <- kmeans(coords_scale, input$days, nstart = 4)
+        votes$cluster <- km$cluster %>% as_factor()
+        
+        return(votes)
+    })
+    
+    # download filtered table
+    output$download1 <- downloadHandler(filename = function() {"pizzas.csv"},
+        content = function(fname) {write.csv(filter_db() %>% select(input$disp_cols), fname)})
+    
+    # display filtered results
+    output$pizza_db <- renderDT({
+    
+        # display table
+        filter_db() %>%
+            select(input$disp_cols) %>%
+            datatable(rownames = FALSE)
+    })
+    
+    
+    ########################### HOURS ########################### 
+    
+    # show hours for filtered results
+    output$download3 <- downloadHandler(
+        filename = function() {
+            "hours.csv"
+        },
+        content = function(fname) {
+            write.csv(
+                filter_db() %>% select(cluster, restaurant, pizza, address, hours) %>% arrange(cluster),
+                fname
+            )
+        }
+    )
+    
+    output$schedule <- renderDataTable({
+        votes <- filter_db()
+        
+        # display table
+        votes %>%
+            select(cluster, restaurant, pizza, address, hours) %>%
+            arrange(cluster) %>%
+            datatable(rownames = FALSE)
+    })
+    
+    ########################### MAP ########################### 
+    output$map <- renderPlotly({
+        input$go
+        
+        votes <- isolate(filter_db())
+        
+        # plot map of clusters
+        
+        p <- votes %>%
+            ggplot() +
+            geom_sf(data = pdx) +
+            geom_sf(data = river_boundaries, fill = "dodgerblue", color = "transparent", alpha = 0.5) +
+            geom_point(
+                aes(x = lon, y = lat, color = cluster, text = paste(
+                    restaurant,
+                    pizza,
+                    paste("Interest Level: ", interest_level),
+                    str_wrap(toppings, width = 35),
+                    str_wrap(address, width = 35),
+                    str_wrap(hours, width = 35),
+                    paste("Slice or Pie? ", whole_slice),
+                    paste("Takeout? ", str_wrap(takeout, width = 35)),
+                    paste("Availability limit? ", availability_limit),
+                    sep = "<br />"
+                )
+                ), shape = 19, size = 2, alpha = 0.7) +
+            geom_text(aes(x = lon, y = lat, label = interest_level)) +
+            theme(axis.title = element_blank())
+        
+        ggplotly(p, tooltip = "text")
+    })
+    
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ pizza week 2024/pizza-week-2024.R
@@ -0,0 +1,175 @@
+library(tidyverse)
+library(rvest)
+library(tidygeocoder)
+library(sf)
+library(googlesheets4)
+
+url <- 'https://everout.com/portland/events/the-portland-mercurys-pizza-week-2024/e170026/'
+webpage <- read_html(url)
+
+# get restaurant and wing names
+restaurants <- html_nodes(webpage, 'h4 a') %>% html_text2()
+pizzas <- html_nodes(webpage, 'h3 a') %>% html_text2()
+
+# list of links to wing descriptions
+rest_links <- html_nodes(webpage, 'h3 a') %>% html_attr('href')
+
+# label dictionary - will be used to name columns
+label_dict <- c("What It's Called:" = "pizza",
+                "What's On It:" = "toppings",                 
+                "What They Say About It:" = "description",
+                "Where and When to Get It:" = "address_hours",      
+                "The Fine Print!" = "fine_print",
+                "Meat or Vegetarian?" = "meat_veggie",
+                "Vegetarian Substitute?" = "veggie_sub",
+                "Vegan Substitute?" = "vegan_sub",
+                "Gluten Free?" = "gf",
+                "Gluten Free Substitute?" = "gf_sub",
+                "Whole Pie or Slice?" = "whole_slice",
+                "Allow Minors?" = "minors",
+                "Allow Takeout?" = "takeout",
+                "Allow Delivery?" = "delivery",
+                "Purchase Limit per Customer?" = "purchase_limit",
+                "Daily Availability Limit?" = "availability_limit")
+
+# loop through pizza links, get info, build pizza data table
+pizza_week <- tibble()
+for (i in 1:length(rest_links)) {
+  url <- rest_links[i]
+  webpage <- read_html(url)
+  
+  
+  info <- html_nodes(webpage, '.description') %>% html_text2() %>% .[1]
+  labels <- str_match_all(string = info, pattern = "(What It's Called:|What It's Called:|What's On It:|What They Say About It:|Where and When to Get It:|The Fine Print!|Meat or Vegetarian\\?|Vegetarian Substitute\\?|Vegan Substitute\\?|Gluten Free\\?|Gluten Free Substitute\\?|Whole Pie or Slice\\?|Allow Minors\\?|Allow Takeout\\?|Allow Delivery\\?|Purchase Limit per Customer\\?|Daily Availability Limit\\?)")[[1]][,2]
+  
+  
+  # split at each label
+  label_list = c()
+  for (label in labels){
+    label <- str_replace(label,"[?]","\\\\?")
+    label_list <- label_list %>% append(label)
+  }
+  label_list <- paste(unlist(label_list), collapse='|')
+  
+  info <- info %>% str_split_1(label_list) %>% .[-1]
+  
+  info2 <- c()
+  for (element in info) {
+    element <- str_squish(element)
+    info2 <- append(info2, str_replace(element, "^ |^[?] ", ""))
+  }
+  
+  # build tibble
+  temp_tib <- tibble(pizza = info2[1])
+  for (j in seq(2,length(info2))) {
+    name <- paste(label_dict[paste0(labels[j][[1]])]) 
+    val <- paste0(info2[j])
+    temp_tib <- temp_tib %>% mutate(!!name := val) %>% mutate(restaurant = restaurants[i])
+  }
+
+  if (dim(pizza_week)[1] == 0) {
+    pizza_week <- pizza_week %>% bind_rows(temp_tib)
+  } else {
+    pizza_week <- pizza_week %>% full_join(temp_tib)
+    }
+  
+  rm(info, info2, url, webpage, labels, label_list, temp_tib, name, val, element, i, j)
+}
+
+##### cleaning #####
+
+# remove restaurant name from start of address_hours
+names <- restaurants %>% append(c("Von Ebert Beaverton", "Von Ebert Cascade Station", "Von Ebert Pearl", "Von Ebert Glendoveer"))
+pizza_week2 <- tibble()
+for (i in seq(1,nrow(pizza_week))) {
+  temp <- pizza_week[i,]
+  ah <- temp$address_hours %>% str_split(", |: ", n = 2)
+  if (ah[[1]][1] %in% names) {
+    temp$address_hours <- ah[[1]][2]
+  }
+  pizza_week2 <- pizza_week2 %>% bind_rows(temp)
+}
+pizza_week <- pizza_week2 %>% select(-fine_print)
+rm(pizza_week2, names, temp, ah, i)
+
+
+
+# duplicate entries if there are two addresses associated
+locations <- read_csv("locations.csv")
+pizza_week <- full_join(pizza_week, locations)
+pizza_week2 <- tibble()
+for (i in seq(1,nrow(pizza_week))) {
+  temp <- pizza_week[i,]
+  if (temp$locations > 1) {
+    loc_sep <- temp$loc_sep
+    loc_split <- str_split(loc_sep, ", ", simplify = T)
+    if (length(loc_split) > 1) {
+      ah <- temp$address_hours %>% 
+        str_split(loc_split[1], simplify = T) %>% .[2] %>% 
+        str_split(loc_split[2], simplify = T)
+      temp1 <- temp %>% mutate(address_hours = ah[1] %>% str_squish())
+      temp2 <- temp %>% mutate(address_hours = ah[2] %>% str_squish())
+      pizza_week2 <- pizza_week2 %>% bind_rows(temp1, temp2)
+    } else {
+      ah <- temp$address_hours %>% str_split(loc_sep, simplify = T)
+      temp1 <- temp %>% mutate(address_hours = ah[1] %>% str_squish())
+      temp2 <- temp %>% mutate(address_hours = ah[2] %>% str_squish())
+      pizza_week2 <- pizza_week2 %>% bind_rows(temp1, temp2)
+      }}
+  else {
+    pizza_week2 <- pizza_week2 %>% bind_rows(temp)
+  }
+}
+pizza_week <- pizza_week2 %>% select(-locations, -loc_sep)
+rm(temp, temp1, temp2, pizza_week2, loc_split, loc_sep, ah, i)
+
+# separate address_hours into address and hours
+# REGEX:
+### Group 1 = Everything
+### Group 2 = Address
+### Group 3 = Hours
+pattern <- "((?:\\d{1,5}) (?:[NSEWest]{1,4}\\.?) (?:[A-z]|\\d|\\s|#)+\\,?\\.? ?(?:Suite \\d{1,5})?(?:Beaverton, OR)?) ?(?:\\/|-|\\.)? ?(.+)"
+pizza_week <- pizza_week %>% mutate(address = str_match(string = address_hours, pattern = pattern)[,2],
+                      hours = str_match(string = address_hours, pattern = pattern)[,3]) %>% 
+  select(-address_hours)
+
+# add city to address
+for (addy in pizza_week$address) {
+  if (str_detect(addy, "Portland|Lake Oswego|Tigard|Beaverton|Hillsboro|Gresham") == FALSE) {
+    temp_tib <- pizza_week %>% filter(address == addy) %>% select(-address) %>% 
+      mutate(address = paste0(addy, ", Portland, OR"))
+  } else if (str_detect(addy, "OR$|\\d$") == FALSE) {
+    temp_tib <- pizza_week %>% filter(address == addy) %>% select(-address) %>% 
+      mutate(address = paste0(addy, ", OR"))
+  } else {
+    temp_tib <- pizza_week %>% filter(address == addy)
+  }
+  pizza_week <- pizza_week %>% filter(address != addy) %>% 
+    bind_rows(temp_tib) %>% 
+    arrange(restaurant)
+  rm(temp_tib)
+}
+
+# arrange columns
+pizza_week <- pizza_week %>% select(pizza, restaurant, toppings, description, address, hours, everything())
+
+# export
+save(pizza_week, file = "pizza_week.RData")
+write_csv(x = pizza_week, file = "pizza_week_2023.csv")
+
+# save to google sheets so we can vote:
+gs4_create("pizza-week-2024", sheets = pizza_week)
+
+
+### for shiny app ###
+# look up gps coordinates
+pizza_week <- pizza_week %>% geocode(address, method = 'arcgis', lat = latitude , long = longitude)
+
+# convert coordinates to mercator
+geometry <- as_tibble(sf_project(st_crs(4326), st_crs(3857), pizza_week %>% select(longitude, latitude)))
+pizza_week <- pizza_week %>% bind_cols(geometry) %>% 
+  rename(lon = V1, lat = V2)
+
+# load pdx gis data
+load("pdx_gis.RData")
+save(pizza_week, pdx, river_boundaries, file = "for_shiny.RData")
